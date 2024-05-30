02129796870393db7e08ebdc01a1dcca0f1cc21f
Frankly Developing <frankly.developing@gmail.com>
2024-05-30 10:52:14 +0200

    Removed unused method

--- Monster Collector/Types/Monster.cs
+++ Monster Collector/Types/Monster.cs
@@ -1,6 +1,5 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
 public class Monster
 {
@@ -21,47 +20,6 @@ public class Monster
         Health = rand.Next(20, 101);
         Attack = rand.Next(10, 51);
         Defense = rand.Next(5, 26);
-    }
-
-    public static string? GenerateNameDescription(Monster monster, List<string> ignoreNames, string? customPrompt = null)
-    {
-        string? output = null;
-
-        try
-        {
-            Console.WriteLine($"Generating name and description for type {monster.Name}");
-
-            string prompt = @$"
-            Generate a unique creative monster name and description for a scary monster in a dungeon game.
-            The description should be no longer than one sentence. The name should be three or less words and be creative and unique.
-            Output the result in the format: Name, Description. The Name and Description must be separated by a comma.
-            The type of monster should be {monster.Name}.
-            {customPrompt ?? ""}
-            The name of the monster can not include any of the following names.";
-
-            // Format the prompt to remove spaces and newlines.
-            prompt = prompt.Replace("\r\n", "").Trim();
-            prompt = Regex.Replace(prompt, @"\s+", " ");
-
-            // Call the LLM.
-            output = new CohereManager().GetTextAsync(prompt, ignoreNames != null && ignoreNames.Count > 0 ? string.Join(", ", ignoreNames) : "null").GetAwaiter().GetResult();
-
-            // Parse the output for "name, description".
-            if (output is not null)
-            {
-                var parts = output.Split(",", 2);
-                monster.Name = parts[0].Trim();
-                monster.Description = parts[1].Trim();
-
-                Console.WriteLine($"Created {monster.Name}, {monster.Description}");    
-            }
-        }
-        catch (Exception excep)
-        {
-            Console.WriteLine(excep.Message);
-        }
-
-        return output;
     }
 
     public override string ToString()
