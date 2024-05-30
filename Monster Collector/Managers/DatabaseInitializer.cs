65b63d8c491c31a3bb7b2cc45af4d51b35461bd7
Frankly Developing <frankly.developing@gmail.com>
2024-05-30 10:38:12 +0200

    Moved DB initialization into DatabaseInitializer.cs

---
+++ Monster Collector/Managers/DatabaseInitializer.cs
@@ -0,0 +1,35 @@
+﻿namespace Monster_Collector.Managers;
+
+public class DatabaseInitializer
+{
+    public void InitializeDatabase()
+    {
+        using var context = new DatabaseContext();
+        if (context.Database.EnsureCreated())
+        {
+            Console.WriteLine("Generating monsters for database.");
+            var cohereManager = new CohereManager();
+            if (!cohereManager.IsValid())
+            {
+                Console.WriteLine("Missing CohereApiKey in .env file. Register an API key at https://dashboard.cohere.com/api-keys");
+            }
+
+            List<string> existingNames = new List<string>();
+
+            // Generate monsters.
+            var monsters = new List<Monster>();
+            for (int i=0; i<10; i++)
+            {
+                var monster = new MonsterFactory().Create(existingNames);
+                monsters.Add(monster);
+
+                // Prevent duplicate names.
+                existingNames.Add(monster.Name);
+            }
+
+            // Add the monsters to the database.
+            context.Monsters.AddRange(monsters);
+            context.SaveChanges();
+        }
+    }
+}
--- Monster Collector/Program.cs
+++ Monster Collector/Program.cs
@@ -1,6 +1,5 @@
 using DotNetEnv;
 using Monster_Collector.Managers;
-using Monster_Collector.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,36 +9,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-void InitializeDatabase()
-{
-    using var context = new DatabaseContext();
-    if (context.Database.EnsureCreated())
-    {
-        Console.WriteLine("Generating monsters for database.");
-        var cohereManager = new CohereManager();
-        if (!cohereManager.IsValid())
-        {
-            Console.WriteLine("Missing CohereApiKey in .env file. Register an API key at https://dashboard.cohere.com/api-keys");
-        }
 
-        List<string> existingNames = new List<string>();
-
-        // Generate monsters.
-        var monsters = new List<Monster>();
-        for (int i=0; i<10; i++)
-        {
-            var monster = new MonsterFactory().Create(existingNames);
-            monsters.Add(monster);
-
-            // Prevent duplicate names.
-            existingNames.Add(monster.Name);
-        }
-
-        // Add the monsters to the database.
-        context.Monsters.AddRange(monsters);
-        context.SaveChanges();
-    }
-}
 
 // Load environment variables.
 Env.Load();
@@ -62,6 +32,6 @@ app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllers();
 
-InitializeDatabase();
+new DatabaseInitializer().InitializeDatabase();
 
 app.Run();
